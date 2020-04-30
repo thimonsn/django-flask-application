4a012c830ff635e797cc534ced7f3b00343ff84b
Matt Hauber <matthauber@Matts-MacBook-Air.local>
2020-04-30 19:28:40 -0400

    prevent users form submitting review twice

--- application.py
+++ application.py
@@ -12,8 +12,6 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-#res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "o817W0fo5miGG9X1d18tzQ", "isbns": "9781632168146"})
-#print(res.json())
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -21,11 +19,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-#Session(app)
-
-# Set up database
-#engine = create_engine(os.getenv("DATABASE_URL"))
-#db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     db.create_all()
@@ -51,7 +44,6 @@ def search():
     search = request.form.get("book")
 
     try:
-        # Category.query.filter_by(title=Category.title.like("category_param_value %"))
         result = Books.query.filter(or_(Books.title.ilike(f"%{search}%"), Books.isbn.ilike(f"%{search}%"),
         Books.author.ilike(f"%{search}%")))
         return render_template("books.html", result=result)
@@ -60,25 +52,33 @@ def search():
 
 @app.route("/search/<isbn>", methods=["GET"])
 def book(isbn):
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+    params={"key": "o817W0fo5miGG9X1d18tzQ", "isbns": "9781632168146"})
+    print(res.json())
     try:
-        # Category.query.filter_by(title=Category.title.like("category_param_value %"))
         result = Books.query.filter_by(isbn=isbn).first()
-        review = Reviews.query.filter_by(book=9)
-        print(result)
+        review = Reviews.query.filter_by(book=result.id)
+
         return render_template("details.html", result=result, review=review)
     except ValueError:
         return "Select Failed"
 
 @app.route("/review", methods=["POST"])
 def review():
-    review = request.form.get("review")
-    rating = request.form.get("rating")
     bookId = request.form.get("bookId")
+    can_review = Reviews.query.filter_by(user=1, book=bookId).first()
+    print(can_review)
 
-    new_review = Reviews(rating, review, 1, bookId)
-    db.session.add(new_review)
-    db.session.commit()
-    return render_template("books.html")
+    if not can_review:
+        review = request.form.get("review")
+        rating = request.form.get("rating")
+
+        new_review = Reviews(rating, review, 1, bookId)
+        db.session.add(new_review)
+        db.session.commit()
+        return render_template("books.html")
+    else:
+        return "User already submitted review"
 
 
 @app.route("/register", methods=["POST"])
