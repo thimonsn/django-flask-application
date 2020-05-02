136d4726f36cbd3cbc130f0d146d7b351725d465
Matt Hauber <matthauber@Matts-MacBook-Air.local>
2020-05-02 00:03:45 -0400

    added goodreads api

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from models import *
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine, or_
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -15,6 +15,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = 'key'
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -35,9 +36,18 @@ def login():
     try:
         user = Users.query.filter_by(username=name).first()
         if user is not None and password == user.password:
+            session['user_id'] = user.id
             return render_template("books.html", user=user)
     except ValueError:
         return "Login Failed"
+
+@app.route("/logout")
+def logout():
+    user = session.get('user_id')
+
+    if user:
+        session.pop('user_id')
+        return render_template("index.html")
 
 @app.route("/search", methods=["POST"])
 def search():
@@ -52,14 +62,23 @@ def search():
 
 @app.route("/search/<isbn>", methods=["GET"])
 def book(isbn):
-    res = requests.get("https://www.goodreads.com/book/review_counts.json",
-    params={"key": "o817W0fo5miGG9X1d18tzQ", "isbns": "9781632168146"})
-    print(res.json())
+    x = isbn
+    print("isbn: " + x)
+    list = [isbn]
+    api = requests.get("https://www.goodreads.com/book/review_counts.json",
+    params={"key": "B30LyGpK2OdNTH7HQlEpA", "isbns": list})
+
+    json = api.json()
+    average_rating = json['books'][0]['average_rating']
+    ratings_count = json['books'][0]['ratings_count']
+    print(json)
+    print("??????????????????????????????????????")
     try:
         result = Books.query.filter_by(isbn=isbn).first()
         review = Reviews.query.filter_by(book=result.id)
+        print("FUCK SHIT" + isbn)
 
-        return render_template("details.html", result=result, review=review)
+        return render_template("details.html", result=result, review=review, average_rating=average_rating, ratings_count=ratings_count)
     except ValueError:
         return "Select Failed"
 
@@ -70,10 +89,11 @@ def review():
     print(can_review)
 
     if not can_review:
+        user = session.get('user_id')
         review = request.form.get("review")
         rating = request.form.get("rating")
 
-        new_review = Reviews(rating, review, 1, bookId)
+        new_review = Reviews(rating, review, user, bookId)
         db.session.add(new_review)
         db.session.commit()
         return render_template("books.html")
